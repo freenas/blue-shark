07bc5a8949a99b273a500c7e71872e48681ab32e
joshuarule <joshua@joshuarule.com>
2016-02-08 02:34:06 -0500

    add checkbox

---
+++ ui/checkbox.reel/checkbox.js
@@ -0,0 +1,10 @@
+/**
+ * @module ui/checkbox.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Checkbox
+ * @extends Component
+ */
+exports.Checkbox = Component.specialize();
