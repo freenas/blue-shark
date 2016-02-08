2b87847950db2c2bc478255652d03fd3a17d69e0
joshuarule <joshua@joshuarule.com>
2016-02-08 00:06:57 -0500

    add radio component and styles. example file not currently working

---
+++ ui/radio-button.reel/radio-button.js
@@ -0,0 +1,10 @@
+/**
+ * @module ui/radio-button.reel
+ */
+var AbstractRadioButton = require("montage/ui/base/abstract-radio-button").AbstractRadioButton;
+
+/**
+ * @class RadioButton
+ * @extends Component
+ */
+exports.RadioButton = AbstractRadioButton.specialize();
