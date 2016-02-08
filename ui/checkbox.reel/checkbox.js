24aadff812c3fdbe461a0aed145e419adcd3dc69
joshuarule <joshua@joshuarule.com>
2016-02-08 17:13:09 -0500

    refactoring checkbox

--- ui/checkbox.reel/checkbox.js
+++ ui/checkbox.reel/checkbox.js
@@ -7,4 +7,14 @@ var Component = require("montage/ui/component").Component;
  * @class Checkbox
  * @extends Component
  */
-exports.Checkbox = Component.specialize();
+exports.Checkbox = Component.specialize({
+
+    enterDocument: {
+        value: function (isFirstTime) {
+            if (isFirstTime) {
+                this.checkboxComponent.element.setAttribute("id", this.uuid);
+                this.labelComponent.element.setAttribute("for", this.uuid);
+            }
+        }
+    }
+});
