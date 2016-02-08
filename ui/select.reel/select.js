14b72d9610f30067709b485581b61afbbda7dd4f
joshuarule <joshua@joshuarule.com>
2016-02-08 00:05:54 -0500

    adding work in progress of select.reel

---
+++ ui/select.reel/select.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/select.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Select
+ * @extends Component
+ */
+exports.Select = Component.specialize(/** @lends Select# */ {
+    constructor: {
+        value: function Select() {
+            this.super();
+        }
+    }
+});
