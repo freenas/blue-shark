5918c1bb87fc79f03ba8479ccf9a46e3e4e69390
joshuarule <joshua@joshuarule.com>
2016-02-15 17:58:11 -0500

    updating select styles for spacing and z-index

--- ui/select.reel/select.js
+++ ui/select.reel/select.js
@@ -7,10 +7,4 @@ var Component = require("montage/ui/component").Component;
  * @class Select
  * @extends Component
  */
-exports.Select = Component.specialize(/** @lends Select# */ {
-    constructor: {
-        value: function Select() {
-            this.super();
-        }
-    }
-});
+exports.Select = Component.specialize();
