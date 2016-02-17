ced22d96ff1eea65b8417c663d8fc715aee22a54
joshuarule <joshua@joshuarule.com>
2016-02-17 15:23:14 -0500

    adds field-select-multiple component

---
+++ ui/field-select-multiple.reel/field-select-multiple.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/field-select-multiple.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class FieldSelectMultiple
+ * @extends Component
+ */
+exports.FieldSelectMultiple = Component.specialize(/** @lends FieldSelectMultiple# */ {
+    constructor: {
+        value: function FieldSelectMultiple() {
+            this.super();
+        }
+    }
+});
