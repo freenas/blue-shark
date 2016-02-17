f65a45b0972ec01e3ebec2ae9efa985fc79882ac
joshuarule <joshua@joshuarule.com>
2016-02-17 15:21:59 -0500

    adds field-checkbox component

---
+++ ui/field-checkbox.reel/field-checkbox.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/field-checkbox.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class FieldCheckbox
+ * @extends Component
+ */
+exports.FieldCheckbox = Component.specialize(/** @lends FieldCheckbox# */ {
+    constructor: {
+        value: function FieldCheckbox() {
+            this.super();
+        }
+    }
+});
