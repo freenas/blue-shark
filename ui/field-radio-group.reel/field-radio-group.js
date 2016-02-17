9780abd2973f8476f476a73cd9dfd517e12ef79f
joshuarule <joshua@joshuarule.com>
2016-02-17 15:22:45 -0500

    adds field-radio-group component

---
+++ ui/field-radio-group.reel/field-radio-group.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/field-radio-group.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class FieldRadioGroup
+ * @extends Component
+ */
+exports.FieldRadioGroup = Component.specialize(/** @lends FieldRadioGroup# */ {
+    constructor: {
+        value: function FieldRadioGroup() {
+            this.super();
+        }
+    }
+});
