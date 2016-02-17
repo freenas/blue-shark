2038f02a731a958ef438e98372cf210b56f65436
joshuarule <joshua@joshuarule.com>
2016-02-17 15:23:43 -0500

    adds field-select component

---
+++ ui/field-select.reel/field-select.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/field-select.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class FieldSelect
+ * @extends Component
+ */
+exports.FieldSelect = Component.specialize(/** @lends FieldSelect# */ {
+    constructor: {
+        value: function FieldSelect() {
+            this.super();
+        }
+    }
+});
