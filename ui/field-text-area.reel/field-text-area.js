5a83db24de49ca695aa447dd36c0987ad8972771
joshuarule <joshua@joshuarule.com>
2016-02-17 15:24:34 -0500

    adds field-text-area component

---
+++ ui/field-text-area.reel/field-text-area.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/field-text-area.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class FieldTextArea
+ * @extends Component
+ */
+exports.FieldTextArea = Component.specialize(/** @lends FieldTextArea# */ {
+    constructor: {
+        value: function FieldTextArea() {
+            this.super();
+        }
+    }
+});
