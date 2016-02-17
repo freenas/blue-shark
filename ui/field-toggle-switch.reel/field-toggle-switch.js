a0baf4ace5a6c0acf8aa34d4e5ebed3dd110c30d
joshuarule <joshua@joshuarule.com>
2016-02-17 15:25:30 -0500

    adds field-text-toggle-switch component

---
+++ ui/field-toggle-switch.reel/field-toggle-switch.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/field-toggle-switch.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class FieldToggleSwitch
+ * @extends Component
+ */
+exports.FieldToggleSwitch = Component.specialize(/** @lends FieldToggleSwitch# */ {
+    constructor: {
+        value: function FieldToggleSwitch() {
+            this.super();
+        }
+    }
+});
