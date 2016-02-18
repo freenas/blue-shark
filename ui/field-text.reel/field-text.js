a4bb69eb5ebf2965594ebddedfcf50d7879e8100
joshuarule <joshua@joshuarule.com>
2016-02-18 18:31:50 -0500

    adds field-text component and hides info description temporarily

---
+++ ui/field-text.reel/field-text.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/field-text.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class FieldText
+ * @extends Component
+ */
+exports.FieldText = Component.specialize(/** @lends FieldText# */ {
+    constructor: {
+        value: function FieldText() {
+            this.super();
+        }
+    }
+});
