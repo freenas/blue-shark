fac8886085d4d5a4f6c92a37edbcbea2ea7ab223
Pierre Chaussalet <pchaussalet@gmail.com>
2016-03-10 13:48:03 -0800

    Fix checkbox uuid generation

--- ui/checkbox.reel/checkbox.js
+++ ui/checkbox.reel/checkbox.js
@@ -1,19 +1,29 @@
 /**
  * @module ui/checkbox.reel
  */
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    Uuid = require("montage/core/uuid");
 
 /**
  * @class Checkbox
  * @extends Component
  */
 exports.Checkbox = Component.specialize({
+    _uuid: {
+        value: null
+    },
+
+    constructor: {
+        value: function() {
+            this._uuid = Uuid.generate();
+        }
+    },
 
     enterDocument: {
         value: function (isFirstTime) {
             if (isFirstTime) {
-                this.checkboxComponent.element.setAttribute("id", this.uuid);
-                this.labelComponent.element.setAttribute("for", this.uuid);
+                this.checkboxComponent.element.setAttribute("id", this._uuid);
+                this.labelComponent.element.setAttribute("for", this._uuid);
             }
         }
     }
