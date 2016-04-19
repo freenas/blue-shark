987b3938568a80e0de305efc60caca565d061610
joshuarule <joshua@joshuarule.com>
2016-04-19 15:21:53 -0700

    adds wip time component

---
+++ ui/time.reel/time.js
@@ -0,0 +1,143 @@
+/**
+ * @module ui/time.reel
+ */
+var Component = require("montage/ui/component").Component,
+    KeyComposer = require("montage/composer/key-composer").KeyComposer;
+
+/**
+ * @class Time
+ * @extends Component
+ */
+exports.Time = Component.specialize(/** @lends Time# */ {
+    enterDocument: {
+        value: function() {
+            if (!this.options) {
+                this.options = [];
+            }
+        }
+    },
+
+    prepareForActivationEvents: {
+        value: function() {
+            this._inputField.delegate = {
+                shouldAcceptValue: function() {
+                    return true;
+                }
+            };
+            KeyComposer.createKey(this._inputField, "down", "down").addEventListener("keyPress", this);
+            KeyComposer.createKey(this._inputField, "up", "up").addEventListener("keyPress", this);
+        }
+    },
+
+    options: {
+        value: null
+    },
+
+    _blurInputField: {
+        value: function () {
+            this._inputField.element.blur();
+        }
+    },
+
+    handleInputAction: {
+        value: function() {
+            console.log(this._inputField);
+            if (this._inputField.value) {
+                this._selectedOption = this._inputField.value;
+                this._blurInputField();
+            }
+        }
+    },
+
+    handleDownKeyPress: {
+        value: function(event) {
+            switch (event.target.component) {
+                case this._inputField:
+                    if (this.options && this.options.length > 0) {
+                        this._navigateInOptions(1)
+                    }
+                    break;
+            }
+        }
+    },
+
+    handleUpKeyPress: {
+        value: function(event) {
+            switch (event.target.component) {
+                case this._inputField:
+                    if (this.options && this.options.length > 0) {
+
+                        this._navigateInOptions(-1);
+                    }
+                    break;
+            }
+        }
+    },
+
+    _stopScrollingOptions: {
+        value: function () {
+            this._optionsController.clearSelection();
+            this._selectedOption = null;
+            this._inputField.value = this._typedValue;
+            this._typedValue = null;
+        }
+    },
+
+    _navigateInOptions: {
+        value: function(distance) {
+            var currentIndex = this._optionsController.organizedContent.indexOf(this._optionsController.selection[0]),
+                newIndex = currentIndex + distance,
+                contentLength = this._optionsController.organizedContent.length;
+            if (newIndex < -1) {
+                newIndex = contentLength -1;
+            }
+            if (newIndex == -1 || newIndex == contentLength) {
+                this._inputField.value = this._typedValue;
+                this._stopScrollingOptions();
+            } else {
+                this._selectOption(this._optionsController.organizedContent[newIndex % contentLength]);
+            }
+        }
+    },
+
+    _selectOption: {
+        value: function (option) {
+            if (!this._typedValue) {
+                this._typedValue = this._inputField.value;
+            }
+            this._optionsController.select(option);
+            this._inputField.value = this._optionsController.selection[0].value;
+            this._selectedOption = option;
+        }
+    },
+
+    __selectedOption: {
+        value: null
+    },
+
+    _selectedOption: {
+        get: function() {
+            return this.__selectedOption;
+        },
+        set: function(option) {
+            if (option && this.__selectedOption != option) {
+                this.__selectedOption = option;
+                this._selectOption(option);
+            }
+        }
+    },
+
+    handleIncrementAction: {
+        value: function (e) {
+            this._navigateInOptions(1);
+            e.preventDefault();
+        }
+    },
+
+    handleDecrementAction: {
+        value: function (e) {
+            this._navigateInOptions(-1);
+            e.preventDefault();
+        }
+    }
+});
