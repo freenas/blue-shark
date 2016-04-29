9153d6ce8f90ea8ce35f6600e82840711f989244
joshuarule <joshua@joshuarule.com>
2016-04-29 16:24:24 -0400

    fix textarea bug Ticket: #14777

--- ui/field-text-area.reel/field-text-area.js
+++ ui/field-text-area.reel/field-text-area.js
@@ -8,9 +8,7 @@ var Component = require("montage/ui/component").Component;
  * @extends Component
  */
 exports.FieldTextArea = Component.specialize(/** @lends FieldTextArea# */ {
-    constructor: {
-        value: function FieldTextArea() {
-            this.super();
-        }
+    enabled: {
+        value: true
     }
 });
