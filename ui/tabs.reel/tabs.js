bee83d931dd8515b332018e886fb45b9649a0c54
Joshua Rule Dobson <joshua@joshuarule.com>
2016-05-16 14:44:33 -0400

    adds tabs component

---
+++ ui/tabs.reel/tabs.js
@@ -0,0 +1,10 @@
+/**
+ * @module ui/tabs.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Tabs
+ * @extends Component
+ */
+exports.Tabs = Component.specialize();
