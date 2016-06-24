2f19b108e59ad609962e78764b70a2a511330166
Joshua Rule Dobson <joshua@joshuarule.com>
2016-06-24 15:14:53 -0400

    updates gulpfile to only process changed _.css files and updated all css files when config is edited

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,11 @@ var path                    = require('path');
     postcss                 = require('gulp-postcss'),
     header                  = require('gulp-header'),
     rename                  = require('gulp-rename'),
+    newer                   = require('gulp-newer'),
     cssnext                 = require('postcss-cssnext'),
     postcssImport           = require('postcss-import'),
     postcssDiscardComments  = require('postcss-discard-comments'),
-    browserSync             = require('browser-sync'),
+    browserSync             = require('browser-sync').create(),
     cssnano                 = require('cssnano');
 
 
@@ -15,7 +16,8 @@ gulp.task('serve', ['css'], function() {
         server: "./"
     });
 
-    gulp.watch("ui/**/_*.css", ['css']);
+    gulp.watch("ui/**.reel/_*.css", ['css']);
+    gulp.watch("ui/_*.css", ['allCss']);
     gulp.watch("ui/**/*.html").on('change', browserSync.reload);
 });
 
@@ -25,17 +27,36 @@ gulp.task('css', function() {
         postcssImport,
         cssnext,
         postcssDiscardComments,
-        cssnano()
+        cssnano({autoprefixer: false})
     ];
 
     return gulp.src('ui/**.reel/_*.css')
+        .pipe(rename(function(path) {
+            path.basename = path.basename.substring(1);
+        }))
+        .pipe(newer({dest: './ui'}))
         .pipe(header("@import '../_config';"))
 
         .pipe(postcss(processors))
+        .pipe(gulp.dest('./ui'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('allCss', function() {
+    var processors = [
+        postcssImport,
+        cssnext,
+        postcssDiscardComments,
+        cssnano({autoprefixer: false})
+    ];
+
+    return gulp.src('ui/**.reel/_*.css')
         .pipe(rename(function(path) {
-            // path.basename = '_' + path.basename;
             path.basename = path.basename.substring(1);
         }))
+        .pipe(header("@import '../_config';"))
+
+        .pipe(postcss(processors))
         .pipe(gulp.dest('./ui'))
         .pipe(browserSync.stream());
 });
