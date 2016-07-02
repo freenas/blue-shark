bc1f0c25043e83a78a3bca6d8d93f868fb1dffb4
Pierre Chaussalet <pchaussalet@gmail.com>
2016-07-02 03:29:30 +0200

    Fix tabs control

--- ui/tabs.reel/tabs.js
+++ ui/tabs.reel/tabs.js
@@ -7,4 +7,38 @@ var Component = require("montage/ui/component").Component;
  * @class Tabs
  * @extends Component
  */
-exports.Tabs = Component.specialize();
+exports.Tabs = Component.specialize({
+    _selectedTab: {
+        value: null
+    },
+
+    selectedTab: {
+        get: function() {
+            return this._selectedTab;
+        },
+        set: function(selectedTab) {
+            if (this._selectedTab !== selectedTab) {
+                this._selectedTab = selectedTab;
+                if (selectedTab) {
+                    this.selection = selectedTab.value;
+                }
+            }
+        }
+    },
+
+    _selection: {
+        value: null
+    },
+
+    selection: {
+        get: function() {
+            return this._selection;
+        },
+        set: function(selection) {
+            if (this._selection !== selection) {
+                this._selection = selection;
+                this.selectedTab = this.options.filter(function(x) { return x.value == selection; })[0];
+            }
+        }
+    }
+});
