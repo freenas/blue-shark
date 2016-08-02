61fb307c68065af64a7ae23ed91a3d9f616dfcce
Thibault Zanini <thibaultzanini@gmail.com>
2016-08-02 12:53:23 -0700

    Remove UUID reference from the checkbox component.

--- ui/checkbox.reel/checkbox.js
+++ ui/checkbox.reel/checkbox.js
@@ -1,43 +1,42 @@
 /**
  * @module ui/checkbox.reel
  */
-var Component = require("montage/ui/component").Component,
-    Uuid = require("montage/core/uuid");
+var Component = require("montage/ui/component").Component;
 
 /**
  * @class Checkbox
  * @extends Component
+ * 
+ * @fixme: need to be refactored
+ * Kind of hacky with the label.
+ * could be just a subclass of checkbox.
+ * 
  */
 exports.Checkbox = Component.specialize({
-    enabled: {
-        value: true
-    },
-
-
-    _checked: {
-        value: false
-    },
-
-    checked: {
-        set: function (checked) {
-            this._checked = !!checked;
-        },
-        get: function () {
-            return this._checked;
-        }
-    },
-
-    _uuid: {
+    
+    _uid: {
         value: null
     },
 
     enterDocument: {
         value: function (isFirstTime) {
             if (isFirstTime) {
-                this._uuid = Uuid.generate();
-                this.checkboxComponent.element.setAttribute("id", this._uuid);
-                this.labelComponent.element.setAttribute("for", this._uuid);
+                this._uid = this.constructor.generateUID();
+                this.checkboxComponent.element.setAttribute("id", this._uid);
+                this.labelComponent.element.setAttribute("for", this._uid);
             }
         }
     }
+}, {
+
+    _lastUID: {
+        value: Date.now()
+    },
+
+    generateUID: {
+        value: function () {
+            return "checkbox-" + ++this._lastUID;
+        }
+    }
+
 });
