ac3c1730fda2b7dbe72dbd4e6aa985b075d28392
Thibault Zanini <thibaultzanini@gmail.com>
2016-08-19 10:47:17 -0700

    Revert "Avoid to handle mutation records when scrolling."
    This reverts commit 4a2c0116fafcc9842bf03153191cb28add09f614.

--- ui/scrollview.reel/scrollview.js
+++ ui/scrollview.reel/scrollview.js
@@ -255,12 +255,10 @@ var Scrollview = exports.Scrollview = Component.specialize({
 
     handleMutations: {
         value: function (mutations) {
-            if (!this.scrollTimeout) {
-                var needsDraw = this.callDelegateMethod("shouldScrollViewComputeBoundaries", this, true, mutations);
+            var needsDraw = this.callDelegateMethod("shouldScrollViewComputeBoundaries", this, true, mutations);
 
-                if (needsDraw === void 0 || needsDraw) {
-                    this.needsDraw = true;
-                }
+            if (needsDraw === void 0 || needsDraw) {
+                this.needsDraw = true;
             }
         }
     },
