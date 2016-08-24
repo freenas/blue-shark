2e07a33e119e1d7c5855cf2807916aedd1a0415f
Thibault Zanini <thibaultzanini@gmail.com>
2016-08-24 11:29:36 -0700

    Removes a workaround from the password field. Issue fixed by using the TextField component from Montage instead of the abstract one.

--- ui/password.reel/password.js
+++ ui/password.reel/password.js
@@ -7,8 +7,4 @@ var Component = require("montage/ui/component").Component;
  * @class Password
  * @extends Component
  */
-exports.Password = Component.specialize(/** @lends Password# */ {
-    enabled: {
-        value: true
-    }
-});
+exports.Password = Component.specialize();
