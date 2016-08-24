2114d365825341a67369426a027ed9a873956f4e
Thibault Zanini <thibaultzanini@gmail.com>
2016-08-24 11:31:04 -0700

    Add password input component.

---
+++ ui/field-password-input.reel/field-password-input.js
@@ -0,0 +1,10 @@
+/**
+ * @module ui/field-password-input.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class FieldPasswordInput
+ * @extends Component
+ */
+exports.FieldPasswordInput = Component.specialize();
