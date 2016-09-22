f65514886ac7df8dd84cfb412bf39dd7f5f12fce
Joshua Rule Dobson <joshua@joshuarule.com>
2016-09-22 16:50:55 -0400

    adds control-error component

---
+++ ui/control-error.reel/control-error.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/control-error.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class ControlError
+ * @extends Component
+ */
+exports.ControlError = Component.specialize(/** @lends ControlError# */ {
+    constructor: {
+        value: function ControlError() {
+            this.super();
+        }
+    }
+});
