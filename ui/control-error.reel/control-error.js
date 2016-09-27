4783496c66aba588c336f12c32ed7dd8ad733438
Pierre Chaussalet <pchaussalet@gmail.com>
2016-09-27 10:37:39 -0700

    Remove useless constructor in control-error.js

--- ui/control-error.reel/control-error.js
+++ ui/control-error.reel/control-error.js
@@ -1,16 +1,3 @@
-/**
- * @module ui/control-error.reel
- */
 var Component = require("montage/ui/component").Component;
 
-/**
- * @class ControlError
- * @extends Component
- */
-exports.ControlError = Component.specialize(/** @lends ControlError# */ {
-    constructor: {
-        value: function ControlError() {
-            this.super();
-        }
-    }
-});
+exports.ControlError = Component.specialize({});
