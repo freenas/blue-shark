dbea00bfd8e9d75c2ffeed21de8547dcb23fb05f
Pierre Chaussalet <pchaussalet@gmail.com>
2016-09-27 10:42:58 -0700

    Remove wrong comments in text-area.js

--- ui/text-area.reel/text-area.js
+++ ui/text-area.reel/text-area.js
@@ -1,10 +1,3 @@
-/**
- * @module ui/control-error.reel
- */
 var Component = require("montage/ui/component").Component;
 
-/**
- * @class ControlError
- * @extends Component
- */
 exports.TextArea = Component.specialize();
