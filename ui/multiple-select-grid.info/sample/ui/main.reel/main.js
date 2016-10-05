17c337beab5dc6a3aa78baf01226c83e1113b635
Joshua Rule Dobson <joshua@joshuarule.com>
2016-10-04 21:24:13 -0400

    refactored multiple-select-grid

--- ui/multiple-select-grid.info/sample/ui/main.reel/main.js
+++ ui/multiple-select-grid.info/sample/ui/main.reel/main.js
@@ -8,36 +8,48 @@ var Component = require("montage/ui/component").Component;
  * @extends Component
  */
 exports.Main = Component.specialize(/** @lends Main# */ {
-    numberContent: {
-        get: function () {
-            var result = [];
-            for (var i = 0; i < 60; i++) {
-                result.push({"value": Number(i, 10)});
-            }
-            return result;
-        }
+    minutesContent: {
+        value: null
     },
     daysContent: {
-        value: [
-            {"index": 1, "label": "M", "value": "monday"},
-            {"index": 0, "label": "S", "value": "sunday"},
-            {"index": 2, "label": "T", "value": "tuesday"},
-            {"index": 3, "label": "W", "value": "wednesday"},
-            {"index": 4, "label": "Th", "value": "thursday"},
-            {"index": 5, "label": "F", "value": "friday"},
-            {"index": 6, "label": "S", "value": "saturday"}
-        ]
+        value: null
     },
 
     selection: {
         value: null
     },
 
+    templateDidLoad: {
+        value: function() {
+            var minutes = [];
+            for (var i = 0; i < 60; i++) {
+                minutes.push({"label": ''+i, "value": i});
+            }
+            this.minutesContent = minutes;/*[
+                {"index": 1, "label": "M", "value": "monday"},
+                {"index": 0, "label": "S", "value": "sunday"},
+                {"index": 2, "label": "T", "value": "tuesday"},
+                {"index": 3, "label": "W", "value": "wednesday"},
+                {"index": 4, "label": "Th", "value": "thursday"},
+                {"index": 5, "label": "F", "value": "friday"},
+                {"index": 6, "label": "S", "value": "saturday"}
+            ];*/
+
+            this.daysContent = [
+                {"index": 1, "label": "M", "value": "monday"},
+                {"index": 0, "label": "S", "value": "sunday"},
+                {"index": 2, "label": "T", "value": "tuesday"},
+                {"index": 3, "label": "W", "value": "wednesday"},
+                {"index": 4, "label": "Th", "value": "thursday"},
+                {"index": 5, "label": "F", "value": "friday"},
+                {"index": 6, "label": "S", "value": "saturday"}
+            ];
+            this.selectedOnOpen = ['monday', 'wednesday'];
+        }
+    },
+
     enterDocument: {
         value: function (isFirstTime) {
-            if (isFirstTime) {
-                this.selectedValues = [this.daysContent[2], this.daysContent[4]];
-            }
         }
     }
 });
--- ui/multiple-select-grid.reel/multiple-select-grid.js
+++ ui/multiple-select-grid.reel/multiple-select-grid.js
@@ -6,222 +6,114 @@ var Component = require("montage/ui/component").Component;
 /**
  * @class MultipleSelectGrid
  * @extends Component
- * @todo: need review with benoit
  */
 exports.MultipleSelectGrid = Component.specialize(/** @lends MultipleSelectGrid# */ {
 
-    constructor: {
-        value: function () {
-            //Fixme: can't bound selection because already bounded.
-            this.addRangeAtPathChangeListener("options.selection", this, "handleOptionSelectionChange");
-        }
-    },
-
-    _iteratorValue: {
+    _sorter: {
         value: null
     },
 
-    iteratorValue: {
-        get: function () {
-            return this._iteratorValue;
-        },
-        set: function (value) {
-            if(this._iteratorValue !== value) {
-                // change to a loop
-                if (value !== 0){
-                    for(var i = 0; i < this.options.iterations.length; i++) {
-                        this.options.iterations[i].selected = false;
-                        if (i % value == 0) {
-                            this.options.iterations[i].selected = true;
-                        }
-                    }
-                }
-                this._iteratorValue = value;
+    _sortedSelection: {
+        get: function() {
+            if (!this._sorter && this._selection && this._selection.length > 0) {
+                this._sorter = typeof this._selection[0].index !== 'undefined' ? this.constructor._indexSorter : this.constructor._valueSorter;
             }
+            return this._selection ? this._selection.slice().sort(this._sorter).map(function(x) { return x.value; }) : [];
         }
-    },
-
-    _cancelSelectionRangeChangeListener: {
-        value: null
-    },
-
-    _selectedValues: {
-        value: null
     },
 
     selectedValues: {
         get: function() {
-
-            return this.selection ? this.selection.map(function(x) { return x.value; }) : [];
-        },
-        set: function(selectedValues) {
-            if (this._selectedValues !== selectedValues) {
-                this._selectedValues = selectedValues;
-                var selection = [];
-                if (this.options && selectedValues) {
-                    var options = this.options.content,
-                        optionsLength = options.length,
-                        j, entry, value;
-                    for (var i = 0, length = selectedValues.length; i < length; i++) {
-                        value = selectedValues[i];
-                        for (j = 0; j < optionsLength; j++) {
-                            entry = options[j];
-                            if (entry.value === value) {
-                                selection.push(entry);
-                                break;
-                            }
-                        }
-                    }
-                }
-                this.selection = selection;
-            }
-        }
-    },
-
-    _selection: {
-        value: null
-    },
-
-    selection: {
-        set: function (selection) {
+            return this._sortedSelection;
+        }, set: function(selectedValues) {
             if (this.options) {
-                var optionSelection = this.options.selection;
-
-                if (optionSelection) {
-                    optionSelection.clear();
-                }
-
-                if (this._selection) {
-                    this._cancelSelectionRangeChangeListener();
-                    this._selection = null;
-                }
-
-                this._selection = selection;
-
-                if (selection) {
-                    //Fixme: @see multiple-select-grid.info
-                    // issue with a 2-way bindings not the same array. (this.options.selection = selection won't work)
-                    this._cancelSelectionRangeChangeListener = (
-                        this.addRangeAtPathChangeListener("_selection", this, "handleSelectionChange")
-                    );
-                } else {
-                    this._cancelSelectionRangeChangeListener = null;
-                }
-            }
-        },
-        get: function () {
-            return this.options ? this.options.selection : null;
-        }
-    },
-
-    handleSelectionChange: {
-        value: function (plus, minus) {
-            if (plus.length) {
-                this.options.selection.addEach(plus);
-            }
-
-            if (minus) {
-                this.options.selection.deleteEach(minus);
-            }
-        }
-    },
-
-    checkForConsistentIteration: {
-        value: function () {
-
-            // @FIXME - attempted to figure out iterationValue from sortedSelection
-
-            // if (this.rangeController && this.sortedSelection.length > 1) {
-            //     var iterationValue = this.sortedSelection[1].index - this.sortedSelection[0].index,
-            //         counter = 0;
-            //     console.log(iterationValue);
-
-            //     for (var i = 0; i < this.sortedSelection.length; i++) {
-
-            //         // take the two values and check that they equal the iterationValue
-
-            //         // if they don't then exit the loop
-
-            //         // check if there is an iteration at the current index plus the iterationValue
-            //         // if not then set the iteration value
-
-
-            //         if (this.sortedSelection[i+1]) {
-
-            //             if (this.sortedSelection[i].index + iterationValue !== this.sortedSelection[i+1].index) {
-            //                 console.log("exit");
-            //                 this.iteratorValue = 0;
-            //                 return false;
-            //             }
-
-            //         }
-            //         // check the last one if there are values after
-            //         if (this.options.iterations[this.sortedSelection[this.sortedSelection.length - 1].index + iterationValue]) {
-            //             console.log("values at end")
-            //             this.iteratorValue = 0;
-            //             return false;
-            //         } else {
-            //             this.iteratorValue = iterationValue;
-            //             console.log("past the end");
-            //         }
-
-            //     }
-            // }
-
-            if (this.rangeController && this.sortedSelection.length > 1 && this.sortedSelection[0].index == 0) {
-                var iterationValue = this.sortedSelection[1].index,
-                    counter = 0;
-
-                for (var i = iterationValue; i < this.options.iterations.length; i++) {
-                    if(this.options.iterations[i].selected && counter == 0) {
-                        // reset counter
-                        counter = iterationValue;
-
-                    } else if (counter == 0 || this.options.iterations[i].selected) {
-                        this.iteratorValue = 0;
-                        return false;
-                    }
-
-                    if (i == this.options.iterations.length - 1) {
-                        this.iteratorValue = iterationValue;
-                    }
-
-                    counter--;
-                }
-            }
-        }
-    },
-
-    handleOptionSelectionChange: {
-        value: function () {
-            this.dispatchOwnPropertyChange("selection", this.selection);
-            this.dispatchOwnPropertyChange("selectedValues", this.selectedValues);
-
-            if(this.hasIterator) {
-                this.checkForConsistentIteration();
+                this._selection = this.options.filter(function(x) {
+                    return selectedValues.indexOf(x.value) != -1;
+                });
             }
         }
     },
 
     enterDocument: {
         value: function() {
-            this.selectedValues = this._selectedValues;
+            this.addPathChangeListener("frequency", this, "_handleFrequencyChange");
+            this._cancelSelectionListener = this.addRangeAtPathChangeListener("_selection", this, "_handleSelectionChange");
         }
     },
 
-    _clearSelection: {
-        value: function () {
-            this.options.iterations.forEach(function(iteration){
-                iteration.selected = false;
-            });
+    exitDocument: {
+        value: function() {
+            this.removePathChangeListener("frequency", this);
+            if (typeof this._cancelSelectionListener === 'function') {
+                this._cancelSelectionListener();
+                this._cancelSelectionListener = null;
+            }
         }
     },
 
     handleClearSelectionButtonAction: {
         value: function () {
-            this._clearSelection();
-            this.iteratorValue = 0;
+            this.controller.clearSelection();
+            this.frequency = null;
+        }
+    },
+
+    _handleSelectionChange: {
+        value: function() {
+            this.frequency = this._getSelectionFrequency();
+        }
+    },
+
+    _handleFrequencyChange: {
+        value: function() {
+            if (this.frequency > 0) {
+                if (this._getSelectionFrequency() !== this.frequency) {
+                    var options = this.controller.organizedContent,
+                        selection = [];
+                    for (var i = 0, length = options.length; i < length; i = i + this.frequency) {
+                        selection.push(options[i]);
+                    }
+                    this._selection = selection;
+                }
+            } else if (typeof this.frequency === 'number') {
+                this.frequency = null;
+            }
+        }
+    },
+
+    _getSelectionFrequency: {
+        value: function() {
+            if (this._selectedIndexes.length > 1) {
+                var i, length,
+                    referenceInterval = this._selectedIndexes[0] === 0 ? this._selectedIndexes[1] - this._selectedIndexes[0] : this._selectedIndexes[0] + 1;
+                for (i = 0, length = this._selectedIndexes.length-1; i < length; i++) {
+                    if (this._selectedIndexes[i+1] - this._selectedIndexes[i] !== referenceInterval) {
+                        return null;
+                    }
+                }
+                var intervalToEnd = this.options.length - this._selectedIndexes[length];
+                if (intervalToEnd > 1 && intervalToEnd !== referenceInterval) {
+                    return null;
+                }
+                return referenceInterval;
+            } else if (this._selectedIndexes.length === 1) {
+                return this.options.length;
+            } else {
+                return null;
+            }
         }
     }
+        
 
+}, {
+    _indexSorter: {
+        value: function(a, b) {
+            return a.index - b.index;
+        }
+    },
+    _valueSorter: {
+        value: function(a, b) {
+            return a.value < b.value ? -1 :  a.value > b.value ? 1 : 0;
+        }
+    }
 });
