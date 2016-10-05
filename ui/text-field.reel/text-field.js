b3aa66276add898e4946de2e0c1a6a038e255dcd
Thibault Zanini <thibaultzanini@gmail.com>
2016-10-05 14:19:35 -0700

    Temporary workaround for controls and converter. Ticket: #18037

--- ui/text-field.reel/text-field.js
+++ ui/text-field.reel/text-field.js
@@ -11,6 +11,41 @@ exports.TextField = TextField.specialize({
 
     hasTemplate: {
         value: true
+    },
+
+    //FIXME: in montage
+    value: {
+        get: function () {
+            return this._value;
+        },
+        set: function (value, fromInput) {
+            if (value !== this._value) {
+                var shouldAcceptValue
+                if (!this.delegate ||  (shouldAcceptValue = this.callDelegateMethod("shouldAcceptValue", this, value) ) === undefined ? true : shouldAcceptValue ){
+                    if (this.converter) {
+                        var convertedValue;
+                        try {
+                            //Where is the matching convert?
+                            convertedValue = this.converter.revert(value);
+                            this.error = null;
+                            this._value = convertedValue;
+                        } catch (e) {
+                            // unable to convert - maybe error
+                            this._value = value;
+                            //FIXME: we don't handle required field.
+                            this.error = value !== "" && value !== void 0 && value !== null ? e : null;
+                        }
+                    } else {
+                        this._value = value;
+                        this.error = null;
+                    }
+
+                    this.callDelegateMethod("didChange", this);
+                    this._elementAttributeValues["value"] = value;
+                    this.needsDraw = true;
+                }
+            }
+        }
     }
 
 });
