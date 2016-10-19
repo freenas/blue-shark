68ab86793780b5dd33c8ece690209b0692515ccc
Joshua Rule Dobson <joshua@joshuarule.com>
2016-10-19 13:03:16 -0400

    adds table structure wip

---
+++ ui/table.reel/table.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/table.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Table
+ * @extends Component
+ */
+exports.Table = Component.specialize(/** @lends Table# */ {
+    constructor: {
+        value: function Table() {
+            this.super();
+        }
+    }
+});
