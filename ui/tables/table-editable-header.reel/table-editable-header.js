515c5a1e4fa1199b3befe646ccea83695b8387f4
Joshua Rule Dobson <joshua@joshuarule.com>
2016-10-19 20:26:43 -0400

    adds table-editable component and table controls overlay

---
+++ ui/tables/table-editable-header.reel/table-editable-header.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/table-editable-header.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class TableEditableHeader
+ * @extends Component
+ */
+exports.TableEditableHeader = Component.specialize(/** @lends TableEditableHeader# */ {
+    constructor: {
+        value: function TableEditableHeader() {
+            this.super();
+        }
+    }
+});
---
+++ ui/tables/table-editable.reel/table-editable.js
@@ -0,0 +1,10 @@
+/**
+ * @module ui/table-editable.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class TableEditable
+ * @extends Component
+ */
+exports.TableEditable = Component.specialize();
