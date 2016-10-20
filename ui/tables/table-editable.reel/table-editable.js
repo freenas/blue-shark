434d52ee75ac69a1d3d8d81f56661fa81e029528
Thibault Zanini <thibaultzanini@gmail.com>
2016-10-19 18:14:55 -0700

    Position the overlay on the bottom-right of its anchor.

--- ui/tables/table-editable.reel/table-editable.js
+++ ui/tables/table-editable.reel/table-editable.js
@@ -7,4 +7,29 @@ var Component = require("montage/ui/component").Component;
  * @class TableEditable
  * @extends Component
  */
-exports.TableEditable = Component.specialize();
+exports.TableEditable = Component.specialize({
+
+    willPositionOverlay: {
+        value: function (overlay, calculatedPosition) {
+            var anchor = overlay.anchor,
+                width = overlay.element.offsetWidth,
+                anchorPosition = anchor.getBoundingClientRect(),
+                anchorHeight = anchor.offsetHeight || 0,
+                anchorWidth = anchor.offsetWidth || 0,
+                position;
+
+            position = {
+                top: anchorPosition.top + anchorHeight,
+                left: anchorPosition.left + (anchorWidth - width)
+            };
+
+            if (position.left < 0) {
+                position.left = 0;
+            }
+
+            return position;
+
+        }
+    }
+
+});
