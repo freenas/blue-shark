47d15b64cf34f5e47645ea7599739e700463a32f
Thibault Zanini <thibaultzanini@gmail.com>
2016-10-31 15:24:15 -0700

    Add selection to read only table.

--- ui/tables/table-read-only.reel/table-read-only.js
+++ ui/tables/table-read-only.reel/table-read-only.js
@@ -7,4 +7,10 @@ var Component = require("montage/ui/component").Component;
  * @class TableReadOnly
  * @extends Component
  */
-exports.TableReadOnly = Component.specialize();
+exports.TableReadOnly = Component.specialize({
+
+    isSelectionEnabled: {
+        value: false
+    }
+
+});
