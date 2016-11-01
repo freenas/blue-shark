b41b2ae08c6b33c551e99d0189e87372ed08a140
Joshua Rule Dobson <joshua@joshuarule.com>
2016-11-01 16:13:22 -0400

    add loading-message component

---
+++ ui/loading-message.reel/loading-message.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/loading-message.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class LoadingMessage
+ * @extends Component
+ */
+exports.LoadingMessage = Component.specialize(/** @lends LoadingMessage# */ {
+    constructor: {
+        value: function LoadingMessage() {
+            this.super();
+        }
+    }
+});
