e626e50e5b8ed6ee7218e578ee8441a92e1e191f
Thibault Zanini <thibaultzanini@gmail.com>
2016-11-10 19:03:21 -0800

    Add field upload file.

---
+++ ui/field-file-upload.reel/field-file-upload.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/field-file-upload.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class FieldFileUpload
+ * @extends Component
+ */
+exports.FieldFileUpload = Component.specialize(/** @lends FieldFileUpload# */ {
+    constructor: {
+        value: function FieldFileUpload() {
+            this.super();
+        }
+    }
+});
