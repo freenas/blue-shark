739ab0fe7ae3da5ba8f56b83204bf6f47100c413
Joshua Rule Dobson <joshua@joshuarule.com>
2016-11-18 15:25:22 -0500

    overrides montage handleKeyPress method...enter key dispatches event on focused button

--- ui/button.reel/button.js
+++ ui/button.reel/button.js
@@ -7,6 +7,18 @@ var Button = require("montage/ui/button.reel").Button;
 var Button = exports.Button = Button.specialize({
     hasTemplate: {
         value: true
+    },
+    handleKeyPress: {
+        value: function (mutableEvent) {
+            // debugger;
+            // when focused action event on spacebar & enter
+            // FIXME - property identifier is not set on the mutable event
+            if (mutableEvent.identifier === "space" ||
+                mutableEvent.identifier === "enter") {
+                this.active = false;
+                this._dispatchActionEvent();
+            }
+        }
     }
 });
 
