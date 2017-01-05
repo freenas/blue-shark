f17fb1254d2417ae7c699e3aa52331163d9dd240
Joshua Rule Dobson <joshua@joshuarule.com>
2017-01-05 10:44:04 -0500

    cleans up loading message js

--- ui/loading-message.reel/loading-message.js
+++ ui/loading-message.reel/loading-message.js
@@ -7,10 +7,4 @@ var Component = require("montage/ui/component").Component;
  * @class LoadingMessage
  * @extends Component
  */
-exports.LoadingMessage = Component.specialize(/** @lends LoadingMessage# */ {
-    constructor: {
-        value: function LoadingMessage() {
-            this.super();
-        }
-    }
-});
+exports.LoadingMessage = Component.specialize();
