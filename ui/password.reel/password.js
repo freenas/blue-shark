441c8f0cec34e9630efa930230325d74c019b97c
Hans Van Mil <hans.vanmil72@gmail.com>
2017-02-17 19:02:56 +0100

    initial commits for password field changes

--- ui/password.reel/password.js
+++ ui/password.reel/password.js
@@ -12,6 +12,34 @@ exports.Password = Component.specialize({
     //FIXME: workaround
     enabled: {
         value: true
-    }
+    },
 
+    __password: {
+        value: null
+    },
+
+    _password: {
+        get: function() {
+            return this.__password;
+        },
+        set: function(strPassword) {
+            if (this.__password !== strPassword) {
+                this.__password = strPassword;
+                this.dispatchOwnPropertyChange('password', this.password);
+            }
+        }
+    },
+
+    password: {
+        get: function() {
+            return this._password ? { $password: this._password } : null;
+        },
+        set: function(password) {
+            if (typeof password === 'object') password = password ? password['$password'] : null;
+            if (this.__password !== password) {
+                this.__password = password;
+                this.dispatchOwnPropertyChange('_password', this._password);
+            }
+        }
+    }
 });
