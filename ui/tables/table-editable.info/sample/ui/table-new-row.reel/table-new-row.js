9d7096d79fd90e5906291c0b635c250463834218
Joshua Rule Dobson <joshua@joshuarule.com>
2017-02-22 16:42:43 -0500

    adds fake converter to sample

--- ui/tables/table-editable.info/sample/ui/table-new-row.reel/table-new-row.js
+++ ui/tables/table-editable.info/sample/ui/table-new-row.reel/table-new-row.js
@@ -7,4 +7,26 @@ var Component = require("montage/ui/component").Component;
  * @class TableNewRow
  * @extends Component
  */
-exports.TableNewRow = Component.specialize();
+exports.TableNewRow = Component.specialize({
+
+    fakeConverter: {
+        value: {
+            convert: function(value) {
+                    return value;
+            },
+            revert: function(value) {
+                if (this.validator(value)) {
+                    return value;
+                }
+            },
+            validator: function (value) {
+                if (+value === parseInt(value)) {
+                    return true;
+                }
+                else {
+                    throw new Error("Value must be an integer.");
+                }
+            }
+        }
+    }
+});
