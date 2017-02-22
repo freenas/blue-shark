c65ce26923f93a1b5c2156730dd9bac9b4630871
Thibault Zanini <thibaultzanini@gmail.com>
2017-02-22 15:13:18 -0500

    Add field search component

---
+++ ui/field-search.reel/field-search.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/field-search.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class FieldSearch
+ * @extends Component
+ */
+exports.FieldSearch = Component.specialize(/** @lends FieldSearch# */ {
+    constructor: {
+        value: function FieldSearch() {
+            this.super();
+        }
+    }
+});
