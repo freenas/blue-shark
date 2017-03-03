e6c14cbd0e26560964632d767269e73670809443
Joshua Rule Dobson <joshua@joshuarule.com>
2017-03-03 14:52:25 -0800

    removes last border on table td causing horizontal scrolling to trigger

--- ui/layouts/table-layout.reel/table-layout.js
+++ ui/layouts/table-layout.reel/table-layout.js
@@ -11,7 +11,7 @@ exports.TableLayout = Component.specialize({
     enterDocument: {
         value: function () {
             if (!!this.contentMaxHeight) {
-                this.scrollview.style.maxheight = this.contentMaxHeight + "em";
+                this.scrollview.style.maxHeight = this.contentMaxHeight + "em";
             }
         }
     }
